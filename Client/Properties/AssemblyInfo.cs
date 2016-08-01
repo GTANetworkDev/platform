b63c689a562536a748560b26fa5e6ccc94c4ce7a
Guad <rockeurp@gmail.com>
2016-08-01 21:01:51 +0200

    code cleanup

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.213.558")]
-[assembly: AssemblyFileVersion("0.1.213.558")]
+[assembly: AssemblyVersion("0.1.213.570")]
+[assembly: AssemblyFileVersion("0.1.213.570")]
