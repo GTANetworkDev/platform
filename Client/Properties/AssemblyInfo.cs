e79ac9f9370b9df9980114dc0183a52064d8f053
Guad <rockeurp@gmail.com>
2016-08-02 21:53:44 +0200

    enabled string caching

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.214.565")]
-[assembly: AssemblyFileVersion("0.1.214.565")]
+[assembly: AssemblyVersion("0.1.214.595")]
+[assembly: AssemblyFileVersion("0.1.214.595")]
