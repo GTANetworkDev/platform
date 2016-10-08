4c8a293d5df30a5bcc31d7b3ba77debecf77c019
Guad <rockeurp@gmail.com>
2016-10-08 12:03:43 +0200

    added menumanger

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.280.631")]
-[assembly: AssemblyFileVersion("0.1.280.631")]
+[assembly: AssemblyVersion("0.1.281.301")]
+[assembly: AssemblyFileVersion("0.1.281.301")]
--- Server/Properties/AssemblyInfo.cs
+++ Server/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.280.609")]
-[assembly: AssemblyFileVersion("0.1.280.609")]
+[assembly: AssemblyVersion("0.1.281.301")]
+[assembly: AssemblyFileVersion("0.1.281.301")]
