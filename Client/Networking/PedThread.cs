f792e07381aef3d3073c9ed5fbd0bf2b9503c6c7
Adam <adambh@outlook.com>
2017-01-26 02:13:50 +0200

    Add Streamer Debug Info on screen option

--- Client/Networking/PedThread.cs
+++ Client/Networking/PedThread.cs
@@ -31,6 +31,10 @@ namespace GTANetwork.Networking
             if (e.KeyCode == Keys.NumPad2) ToggleNametag = !ToggleNametag;
         }
 #endif
+
+        private static int position = 400;
+        private static int offset = 25;
+        private static int n = 1;
         public static void OnTick(object sender, EventArgs e)
         {
 
@@ -63,12 +67,24 @@ namespace GTANetwork.Networking
 #endif
             if(StreamerDebug)
             {
-                Util.Util.DrawText("Stream Loop: " + sw.ElapsedMilliseconds + " ms", 5, 460, 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
-                Util.Util.DrawText("Spawned Vehicles: " + GTA.World.GetAllVehicles().Length + "", 5, 480, 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
-                Util.Util.DrawText("Spawned Peds: " + GTA.World.GetAllPeds().Length + "", 5, 500, 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
-                Util.Util.DrawText("InRange Players: " + InRangePlayers + "", 5, 520, 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
-                Util.Util.DrawText("Streamed Players: " + StreamerThread.StreamedInPlayers.Length + "", 5, 540, 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
-                Util.Util.DrawText("Stream InProgress: " + StreamerThread.StreamInProgress + "", 5, 580, 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Rendering Latency: " + sw.ElapsedMilliseconds + " ms", 5, position, 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                n++;
+                Util.Util.DrawText("Streamed Players: " + (StreamerThread.StreamedInPlayers.Length + 1) + "/" + (StreamerThread.StreamedInPlayers.Length + StreamerThread.StreamedOutPlayers + 1), 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Streamed Entities: " + StreamerThread.StreamedInItems + "/" + (StreamerThread.StreamedInItems + StreamerThread.StreamedOutItems), 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Streamed Vehicles: " + StreamerThread.StreamedInVehicles + "/" + (StreamerThread.StreamedInVehicles + StreamerThread.StreamedOutVehicles), 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Streamed Objects: " + StreamerThread.StreamedInObjects + "/" + (StreamerThread.StreamedInObjects + StreamerThread.StreamedOutObjects), 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Streamed Labels: " + StreamerThread.StreamedInLabels + "/" + (StreamerThread.StreamedInLabels + StreamerThread.StreamedOutLabels), 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Streamed Pickups: " + StreamerThread.StreamedInPickups + "/" + (StreamerThread.StreamedInPickups + StreamerThread.StreamedOutPickups), 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Streamed Blips: " + StreamerThread.StreamedInBlips + "/" + (StreamerThread.StreamedInBlips + StreamerThread.StreamedOutBlips), 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Streamed Particles: " + StreamerThread.StreamedInParticles + "/" + (StreamerThread.StreamedInParticles + StreamerThread.StreamedOutParticles), 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Streamed API Peds: " + StreamerThread.StreamedInPeds + "/" + (StreamerThread.StreamedInPeds + StreamerThread.StreamedOutPeds), 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                n++;
+                Util.Util.DrawText("Spawned Peds: " + GTA.World.GetAllPeds().Length + "", 5, (position + (offset * n++)), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Spawned Vehicles: " + GTA.World.GetAllVehicles().Length + "", 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Spawned Blips: " + GTA.World.GetAllBlips().Length + "", 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Spawned Props: " + GTA.World.GetAllProps().Length + "", 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                Util.Util.DrawText("Spawned Entities: " + GTA.World.GetAllEntities().Length + "", 5, position + (offset * n++), 0.35f, 255, 255, 255, 255, 0, 0, false, true, 0);
+                n = 1;
             }
 
             //Util.Util.DrawText("CurrentStreamedPlayersInRange: " + StreamedPlayersInRange + "", 600, 920, 0.5f, 255, 255, 255, 255, 0, 1, false, true, 0);
