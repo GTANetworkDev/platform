5580ff274713b6c159aaee89d6dbdbe0645ede23
T3charmy <t3charmy@gmail.com>
2017-05-08 14:22:00 -0500

    * Fixed up build number versioning to be setup correctly... for VS2017 at least....

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.493.141")]
-[assembly: AssemblyFileVersion("0.1.493.141")]
+[assembly: AssemblyVersion("0.1.493.578")]
+[assembly: AssemblyFileVersion("0.1.493.578")]
